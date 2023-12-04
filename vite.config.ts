43a568dba7132fe10ec2ce31cc953e54d3595b64
Seojisoo20191941 <amy0480@naver.com>
2023-12-05 05:47:43 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    svgr({
+      svgrOptions: {
+        // svgr options
+      },
+    }),
+    vanillaExtractPlugin({
+      // configuration
+    })
+  ],
+  optimizeDeps: {
+    exclude: ["js-big-decimal"],
+  },
+});
